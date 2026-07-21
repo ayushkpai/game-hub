da68e3371dc4bf97aa9b68a8192b444eb5adce6c
Ayush Pai <ayushkpai@users.noreply.github.com>
2026-07-21 14:16:46 +0530

    imporve layout

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,13 +1,19 @@
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
-import React from "react";
-import { useParams } from "react-router-dom";
-import CriticScore from "../components/CriticScore";
-import DefinitionItem from "../components/DefinitionItem";
-import ExpandableText from "../components/ExpandableText";
-import GameAttributes from "../components/GameAttributes";
-import useGame from "../hooks/useGame";
-import GameTrailer from "../components/GameTrailer";
-import GameScreenshots from "../components/GameScrenshots";
+import {
+  GridItem,
+  Heading,
+  SimpleGrid,
+  Spinner,
+  Text,
+} from '@chakra-ui/react';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import CriticScore from '../components/CriticScore';
+import DefinitionItem from '../components/DefinitionItem';
+import ExpandableText from '../components/ExpandableText';
+import GameAttributes from '../components/GameAttributes';
+import GameScreenshots from '../components/GameScreenshots';
+import GameTrailer from '../components/GameTrailer';
+import useGame from '../hooks/useGame';
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -18,13 +24,17 @@ const GameDetailPage = () => {
   if (error || !game) throw error;
 
   return (
-    <>
-      <Heading>{game.name}</Heading>
-      <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
-      <GameScreenshots gameId={game.id} />
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <ExpandableText>{game.description_raw}</ExpandableText>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game.id} />
+        <GameScreenshots gameId={game.id} />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
