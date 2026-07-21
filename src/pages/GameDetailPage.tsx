8f9d389918174d5667f0d8a99979b6b9d964bff0
Ayush Pai <ayushkpai@users.noreply.github.com>
2026-07-21 13:55:36 +0530

    game attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,8 +1,16 @@
-import { Heading, Spinner, Text } from "@chakra-ui/react";
-import React from "react";
-import { useParams } from "react-router-dom";
-import useGame from "../hooks/useGame";
-import ExpandableText from "../components/ExpandableText";
+import {
+  Heading,
+  SimpleGrid,
+  Spinner,
+  Text,
+} from '@chakra-ui/react';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import CriticScore from '../components/CriticScore';
+import DefinitionItem from '../components/DefinitionItem';
+import ExpandableText from '../components/ExpandableText';
+import GameAttributes from '../components/GameAttributes';
+import useGame from '../hooks/useGame';
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -16,6 +24,7 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
