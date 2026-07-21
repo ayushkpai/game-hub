2306c40d13117370ebe22436d60caba1b072e596
Ayush Pai <ayushkpai@users.noreply.github.com>
2026-07-21 13:17:47 +0530

    expandable text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,11 +1,12 @@
-import { Heading, Spinner, Text } from '@chakra-ui/react';
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import useGame from '../hooks/useGame';
+import { Heading, Spinner, Text } from "@chakra-ui/react";
+import React from "react";
+import { useParams } from "react-router-dom";
+import useGame from "../hooks/useGame";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetailPage = () => {
-  const {slug} = useParams();
-  const {data: game, isLoading, error } = useGame(slug!);
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useGame(slug!);
 
   if (isLoading) return <Spinner />;
 
@@ -14,9 +15,9 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
-  )
-}
+  );
+};
 
-export default GameDetailPage
+export default GameDetailPage;
